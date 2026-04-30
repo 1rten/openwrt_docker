d86311f7e8762ed98cf030830ecd70982cdf9f6a
1rten <yl100124@gmail.com>
2026-04-30 22:43:01 +0800

    chore: Update Docker build configuration and modify DIY script for OpenWrt image

--- diy.sh
+++ diy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# 1. Add Argon Theme source
+# 1. Update feeds first to make sure indexes are available
+./scripts/feeds update -a
+
+# 2. Add Argon Theme (Switch to master branch for 24.10 compatibility)
 rm -rf package/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+rm -rf package/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
-# 2. Update and install feeds
-./scripts/feeds update -a
+# 3. Install all feeds
 ./scripts/feeds install -a
 
-# 3. Modify default IP (Optional, since we use files/ folder, but good as fallback)
+# 4. Modify default settings
+# Change default IP
 sed -i 's/192.168.1.1/192.168.50.199/g' package/base-files/files/bin/config_generate
-
-# 4. Modify default hostname
+# Change default hostname
 sed -i 's/ImmortalWrt/OpenWrt-Docker/g' package/base-files/files/bin/config_generate
-
-# 5. Set default timezone to Asia/Shanghai
+# Change default timezone
 sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate

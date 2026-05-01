b3f7c10a6ad126cf48a1be88e232ca699fb632f2
1rten <yl100124@gmail.com>
2026-05-01 19:31:45 +0800

    chore: Update configuration to replace SSR Plus with PassWall 2 and adjust DIY script comments

--- diy.sh
+++ diy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# 1. Add helloworld feed for SSR Plus
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-
-# 2. Update feeds first to make sure indexes are available
+# 1. Update feeds first to make sure indexes are available
 ./scripts/feeds update -a
 
-# 3. Add Argon Theme (Switch to master branch for 24.10 compatibility)
+# 2. Add Argon Theme (Switch to master branch for 24.10 compatibility)
 rm -rf package/luci-theme-argon
 rm -rf package/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
-# 4. Install all feeds
+# 3. Install all feeds
 ./scripts/feeds install -a
 
 # 4. Modify default settings
